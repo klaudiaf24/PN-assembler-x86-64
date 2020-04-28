fe6e88dc012b3c302ce7d6f219cd0bc3f9fe176f
Klaudia <44114240+klaudiaf24@users.noreply.github.com>
2020-04-28 10:30:17 +0200

    Update run_fil_klaudia.sh

--- run_fil_klaudia.sh
+++ run_fil_klaudia.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-if [ $1 = "clone" ]; then
-	echo ################# Klonowanie repozytorium ################
-    git clone https://github.com/klaudiaf24/PN-assembler-x86-64.git
+command=$1
 
-elif [ $1 = "run" ]; then
-	echo ################# Uruchamienie dockera ################
-	sudo docker build -t klaudiafil:1.0 .
-	sudo docker run -it klaudiafil:1.0
-    
-elif [ $1 = "clean" ]; then
-	echo ################# Usuwanie ################
-   	rm -rf Dockerfile Prezentacja.pdf assembler_zad przyklady_prezentacja Instrukcja.pdf
+#clone
+if [ "$command" = "clone" ]; then
+    echo ###### Klonowanie repozytorium ######
+    git clone https://github.com/klaudiaf24/PN-assembler-x86-64.git
 fi
 
+if [ "$command" = "run" ]; then
+    echo ###### Uruchamianie dockera ######
+    echo W przypadku trudnosci nalezy uruchomić :
+    echo sudo grouadd docker
+    echo sudo usermod -aG docker \$USER
+    echo sudo chown \"\$USER\":\"\$USER\" /home/\"\$USER\"/.docker -R
+    echo sudo chmod g+rwx \"/home/\$USER/.docker\" -T
+    echo newgrp docker
+    echo 
+    echo 
+    if [[ -d "PN-assembler-x86-64" ]]
+    then
+        cd PN-assembler-x86-64
+    fi
+    docker build -t klaudiafil:1.0 .
+    docker run -it klaudiafil:1.0
+fi
+
+if [ "$command" = "clean" ]; then
+    if [[ ! -d "PN-assembler-x86-64" ]]
+    then
+        cd .. 
+    fi
+    echo ###### Usuwanie #######
+    rm -rf PN-assembler-x86-64
+    docker ps -a -q -f status=exited | xargs docker rm
+    docker rmi klaudiafil:1.0 
+    docker rmi ubuntu:20.04
+fi
